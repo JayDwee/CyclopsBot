3ee64fc86aebcae90be92d1f2a3d7f549584ecfa
Jack Draper <jd4g18@soton.ac.uk>
2019-06-20 01:28:29 +0100

    Processes are now saved on config.ini

--- cogs/processDisplay.py
+++ cogs/processDisplay.py
@@ -11,6 +11,11 @@ ToDo
 # Futures
 
 # Built-in/Generic Imports
+import os
+import sys
+import configparser
+import shutil
+import time
 
 # Libs
 import discord
@@ -29,10 +34,31 @@ __email__ = "thejaydwee@gmail.com"
 __status__ = "Development"
 # "Prototype", "Development", or "Production"
 
+# Checks for config file
+if not os.path.exists("./configs/config.ini"):
+    print("No config file can be found in ./configs/.")
+    sys.exit("No config found.")
+# Runs config file
+config = configparser.ConfigParser()
+try:
+    config.read(os.path.abspath("./configs/config.ini"))
+except FileNotFoundError:
+    try:
+        #shutil.copyfile("./configs/default_config.ini", "./configs/config.ini")
+        print("You need to set up the config file correctly.")
+    except shutil.Error:
+        print("Something is wrong with the default config file or the config folder.")
+        time.sleep(4)
+    sys.exit()
+
 # Constants
-ADMIN_ROLE = 'Founders'
-TEXT_CHANNEL = 590459824437985300
-processes = {"cmd.exe": "Command Prompt", "chrome.exe": "Chrome", "Discord.exe": "Discord", "Steam.exe": "Steam"}
+ADMIN_ROLE = config["Credentials"]["admin_role"]
+TEXT_CHANNEL = eval(config["ProcessDisplay"]["text_channel_id"])
+PROCESSES = eval(config["ProcessDisplay"]["processes"])
+DEFAULT_EMBED = discord.Embed(
+            title=":desktop: Program Status",
+            colour=discord.Colour.blue()
+        )
 
 
 class ProcessDisplay(commands.Cog):
@@ -57,10 +83,10 @@ class ProcessDisplay(commands.Cog):
         """
         channel = self.client.get_channel(TEXT_CHANNEL)
 
-        msg = await self.delete_bot_msg(channel)
-        print(msg)
+        await self.delete_bot_msg(channel)
+        msg = await channel.send(embed=DEFAULT_EMBED)
         self.find_processes.start(msg)
-        print("Bot online2")
+        print("ProcessDisplay Running")
 
     # Commands
     @commands.command()
@@ -75,14 +101,18 @@ class ProcessDisplay(commands.Cog):
         :param name: The name to be displayed for the process (e.g. 'Command Prompt')
         :return:
         """
-        if process in processes.keys():
+        if process in PROCESSES.keys():
             await ctx.send(f"The process {process} is already being displayed")
 
-        elif name in processes.values():
+        elif name in PROCESSES.values():
             await ctx.send(f"The process name {name} is already being displayed")
 
         else:
-            processes[process] = name
+            PROCESSES[process] = name
+            config.set("ProcessDisplay", "processes", str(PROCESSES))
+            with open('./configs/config.ini', 'w') as configfile:
+                config.write(configfile)
+            await ctx.send(f"The process {name} has been added")
 
     @commands.command()
     @commands.has_role(ADMIN_ROLE)
@@ -94,9 +124,13 @@ class ProcessDisplay(commands.Cog):
         :param name: Name displayed for the process (e.g. Command Prompt)
         :return:
         """
-        for process in processes.keys():
-            if processes.get(process) == name:
-                processes.pop(process)
+        for process in PROCESSES.keys():
+            if PROCESSES.get(process) == name:
+                PROCESSES.pop(process)
+                config.set("ProcessDisplay", "processes", str(PROCESSES))
+                with open('./configs/config.ini', 'w') as configfile:
+                    config.write(configfile)
+                await ctx.send(f"The process {name} has been removed")
 
     @tasks.loop(seconds=1)
     async def find_processes(self, msg):
@@ -107,23 +141,19 @@ class ProcessDisplay(commands.Cog):
         :return:
         """
         running_processes = []
-        new_embed = discord.Embed(
-            title=":desktop: Program Status",
-            colour=discord.Colour.blue()
-        )
+        new_embed = DEFAULT_EMBED.copy()
 
         for proc in psutil.process_iter():
-            if proc.name() in processes.keys():
+            if proc.name() in PROCESSES.keys():
                 running_processes.append(proc.name())
 
-        for process in processes:
+        for process in PROCESSES:
             if process in running_processes:
-                new_embed.add_field(name=processes.get(process), value="Online :white_check_mark:")
+                new_embed.add_field(name=PROCESSES.get(process), value="Online :white_check_mark:")
             else:
-                new_embed.add_field(name=processes.get(process), value="Offline <:red_cross:590500648639004673>")
+                new_embed.add_field(name=PROCESSES.get(process), value="Offline <:red_cross:590500648639004673>")
 
         await msg.edit(content="", embed=new_embed)
-
 
     def is_me(self, m):
         """
@@ -139,8 +169,7 @@ class ProcessDisplay(commands.Cog):
         :param channel: The channel that will have the messages deleted
         :return: the message that says how many messages were deleted
         """
-        deleted = await channel.purge(limit=100, check=self.is_me)
-        return await channel.send('Deleted {} message(s)'.format(len(deleted)))
+        await channel.purge(limit=100, check=self.is_me)
 
 
 def setup(client):
